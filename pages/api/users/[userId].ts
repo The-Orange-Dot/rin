12682170f71a0e3f9f54840dccefb5bf729165ba
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-07-28 13:40:15 +0900

    added review creating logic and review page

--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -35,7 +35,7 @@ export default async function handler(
         image: true,
         buyHistory: {
           orderBy: { createdAt: "desc" },
-          include: { review: true },
+          select: { review: true },
         },
       },
     });
