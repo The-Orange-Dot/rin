634ee6dcc7d489aa586c5469f089a8ec32b2041c
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-07-29 22:20:51 +0900

    Forgot to include id on more reviews

--- pages/api/productReviews/[productId].ts
+++ pages/api/productReviews/[productId].ts
@@ -23,6 +23,7 @@ export default async function handler(
             rating: true,
             createdAt: true,
             updatedAt: true,
+            id: true,
           },
         },
       },
