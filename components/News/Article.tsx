4be6f9cc349b3e14b646e692e3d4050253b7b1ff
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-08-04 18:30:59 +0900

    Trying to get rid of prrendering error

--- components/News/Article.tsx
+++ components/News/Article.tsx
@@ -2,9 +2,10 @@ import { Box } from "@mui/material";
 import React from "react";
 
 const Article = ({ post }: any) => {
+  console.log(post);
   return (
     <Box>
-      <Box>{post.title}</Box>
+      <Box>Test</Box>
     </Box>
   );
 };
