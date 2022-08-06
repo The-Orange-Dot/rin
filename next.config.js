f4c2ace2eb14f6612d00aaf4446a29f2991d1763
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-08-06 09:29:25 +0900

    Added more security stuff?

--- next.config.js
+++ next.config.js
@@ -8,17 +8,51 @@ const ContentSecurityPolicy = `
   font-src 'self';  
 `;
 
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Server",
+    value: "Apache", // phony server value
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "sameorigin",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=*", // allow specified policies here
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
+        source: "/(.*)",
+        headers: securityHeaders,
       },
     ];
   },
