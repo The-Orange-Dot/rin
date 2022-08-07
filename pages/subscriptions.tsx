b87121c106cc8dbdc7cb00f0b3c2e8887ceee4a5
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-08-07 23:08:06 +0900

    got rid of subscription page

--- pages/subscriptions.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles/subscriptions.module.css";
-
-const subscriptions = () => {
-  return <div className={styles.main}>subscriptions</div>;
-};
-
-export default subscriptions;
